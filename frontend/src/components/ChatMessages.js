7e43db6011f959d35f4a3946d086bca7913b9c3c
Rishabh <rishabhkr.073@gmail.com>
2024-03-29 20:25:01 +0530

    updating backend server endpoint

--- frontend/src/components/ChatMessages.js
+++ frontend/src/components/ChatMessages.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import ScrollableChat from './ScrollableChat';
 import io from 'socket.io-client'
 
-const ENDPOINT = "http://localhost:5000/";
+const ENDPOINT = "https://chatup-vv7c.onrender.com/";
 var socket, selectedChatCompare;
 
 const ChatMessages = () => {
