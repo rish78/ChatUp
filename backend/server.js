63668b1a9326f5d8f850c7be25563937a871c2e4
Rishabh <rishabhkr.073@gmail.com>
2024-03-29 20:16:12 +0530

    fixing deployment

--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1, "/frontend/build")))
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-    })
-}
-else{
+
     app.get('/', (req, res) =>{
       res.status(200).send("Let's go!");
   })
-}
+
 
 
 const port = process.env.PORT || 5000;
